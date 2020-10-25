1440d8e9ea9c4d427337ed947e6c924801c702e7
ingan <isabelngan27@gmail.com>
2020-10-24 20:00:19 -0400

    fixing serverside stuff for heroku

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
 const PORT = process.env.PORT || 3002;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(PORT, function() {
     console.log(`🌎  ==>  Server now listening on PORT ${PORT}!`);
   });
