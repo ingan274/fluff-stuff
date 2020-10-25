da34ba9c7079207c0593d857cf83447b8e403a20
ingan <isabelngan27@gmail.com>
2020-10-24 20:36:35 -0400

    adding remove in cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -117,68 +117,132 @@ class Cart extends Component {
 
         }
     };
-    // Remove item from cart
-    removeItem = (event) => {
-        let number = event.target.attributes.quant.value;
-        const index = event.target.attributes.name.value;
-        const item = this.state.products[index]
-        const removeItem = item.type;
-        let removeColor;
-        let removeFill;
 
-        let localStore = JSON.parse(localStorage.getItem("Cart"));
+    componentDidUpdate = () => {
+        const type = ['bed pillow', 'couch pillow', 'round pillow', 'floor pouf pillow'];
+        const colors = ['ASS', 'MH', 'CD', 'RD'];
+        const fills = ['DD', 'HPB', 'MF']
 
-        switch (item.color) {
-            case ("After School Special"):
-                removeColor = "ASS";
-                break;
-            case ("Morning Haze"):
-                removeColor = "MH";
-                break;
-            case ("Cozy Denim"):
-                removeColor = "CD";
-                break;
-            case ("Rainy Day"):
-                removeColor = "RD";
-                break;
-            default:
-                removeColor = "ASS"
+        if (JSON.parse(localStorage.getItem("Cart")) === null) {
+            this.setState({
+                products: [],
+                cartQuant: 0,
+                productTotal: "00.00",
+                tax: "00.00",
+                grandTotal: "00.00"
+            })
         }
+        // get from local storage, sort, and reorgnize 
+        else {
+            let reOrg = [];
+            let cartTotal = 0;
+            let productTotal = 0;
+            let localStore = JSON.parse(localStorage.getItem("Cart"));
 
-        switch (item.fill) {
-            case ("Duck Down"):
-                removeFill = "DD";
-                break;
-            case ("Hypoallergenic Poly-Blend"):
-                removeFill = "HPB";
-                break;
-            case ("Memory Foam"):
-                removeFill = "MF";
-                break;
-            default:
-                removeFill = "DD"
-        }
+            for (let pillow of type) {
+                let products = localStore.filter(items => items.type === pillow);
+                if (products.length === 0) { continue }
 
-        while (number > 0) {
-            for (let i = 0; i < localStore.length; i++) {
-                if (localStore[i].type === removeItem && localStore[i].color === removeColor && localStore[i].fill === removeFill) {
-                    localStore.splice(i, 1)
-                    number -= 1
+                for (let singleColor of colors) {
+                    let color = products.filter(items => items.color === singleColor);
+                    if (color.length === 0) { continue }
+
+                    for (let singleFill of fills) {
+                        let fill = color.filter(items => items.fill === singleFill);
+                        if (fill.length === 0) { continue }
+                        else if (fill.length > 0) {
+
+                            const prod_type = fill[0].type;
+                            let prod_color;
+                            let prod_fill;
+                            const prod_imgSrc = fill[0].imgSrc;
+                            const prod_imgSize = fill[0].imgSize;
+                            const prod_imgPosition = fill[0].imgPosition;
+                            const prod_cost = fill[0].cost;
+
+                            switch (fill[0].color) {
+                                case ("ASS"):
+                                    prod_color = "After School Special";
+                                    break;
+                                case ("MH"):
+                                    prod_color = "Morning Haze";
+                                    break;
+                                case ("CD"):
+                                    prod_color = "Cozy Denim";
+                                    break;
+                                case ("RD"):
+                                    prod_color = "Rainy Day";
+                                    break;
+                                default:
+                                    prod_color = "After School Special"
+                            }
+
+                            switch (fill[0].fill) {
+                                case ("DD"):
+                                    prod_fill = "Duck Down";
+                                    break;
+                                case ("HPB"):
+                                    prod_fill = "Hypoallergenic Poly-Blend";
+                                    break;
+                                case ("MF"):
+                                    prod_fill = "Memory Foam";
+                                    break;
+                                default:
+                                    prod_fill = "Duck Down"
+                            }
+
+                            let prod_quant = 0;
+                            for (let item of fill) {
+                                let theQuant = item.quant;
+                                prod_quant += theQuant;
+                                cartTotal += theQuant
+
+                            }
+                            const prod_total = prod_cost * prod_quant;
+
+
+
+                            const item = { 'quant': prod_quant, 'type': prod_type, 'color': prod_color, 'fill': prod_fill, "imgSrc": prod_imgSrc, "imgSize": prod_imgSize, "imgPosition": prod_imgPosition, "cost": prod_cost, "totalCost": prod_total }
+                            reOrg.push(item)
+                        }
+                    }
                 }
             }
+
+            for (let productType of reOrg) {
+                productTotal += productType.totalCost;
+            }
+
+            const tax = (productTotal * 0.06).toFixed(2);
+            const totalOfBag = (productTotal + parseFloat(tax)).toFixed(2);
+            productTotal = productTotal.toFixed(2);
+
+            this.setState({
+                products: reOrg,
+                cartQuant: cartTotal,
+                productTotal: productTotal,
+                tax: tax,
+                grandTotal: totalOfBag
+            })
+
         }
+    }
+    // Remove item from cart
+    removeItem = (event) => {
+        const index = event.target.attributes.name.value;
+        const products = this.state.products
+        products.splice(index, 1);
+        let quant = this.state.totalQuant - products[index].quant
 
-        // updating state
-        let newList = this.state.products;
-        newList.splice(index, 1)
-        this.setState((prevState => ({
-            products: newList,
-            cartQuant: prevState - 1
-        })))
+        this.setState({
+            products: products,
+            cartQuant: quant,
+        }, function () {
+            this.cartItems();
+        })
 
-        console.log(localStore)
-        localStorage.setItem("Cart", JSON.stringify(localStore));
-
+        localStorage.setItem("Cart", JSON.stringify(products));
+        this.props.cartUpdate();
     }
 
     // If there are items, Else no cart message
